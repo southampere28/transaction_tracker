070d5afdb7220ce3a6f9e959fd753c609b5dad74
Pramudya <pramudyap333@gmail.com>
2024-12-29 23:10:01 +0700

    adding theme

---
+++ lib/theme.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color primaryColor = Color(0xff0EA5E9);
+const Color secondaryColor = Color(0xff38ABBE);
+const Color thirdColor = Color(0xff018DCD);
+const Color liteblueColor = Color(0xffE7F6FD);
+const Color alertColor = Color(0xffED6363);
+const Color priceColor = Color(0xff2C96F1);
+const Color backgroundColor1 = Color(0xff1F1D2B);
+const Color backgroundColor2 = Color(0xff2B2937);
+const Color backgroundColor3 = Color(0xff242231);
+const Color primaryTextColor = Color(0xffF1F0F2);
+const Color secondaryTextColor = Color(0xff545454);
+const Color subtitleColor = Color(0xff504F5E);
+const Color transparentColor = Colors.transparent;
+const Color blackColor = Color(0xff2E2E2E);
+const Color c4Color = Color(0xffC4C4C4);
+const Color bgCardMentoring = Color(0xffF5F5F5);
+
+ TextStyle primaryTextStyle = GoogleFonts.poppins(
+    color: primaryColor,
+  );
+
+ TextStyle secondaryTextStyle = GoogleFonts.poppins(
+    color: secondaryTextColor,
+  );
+
+ TextStyle titleTextStyle = GoogleFonts.poppins(
+    color: thirdColor,
+  );
+
+ TextStyle whiteTextStyle = GoogleFonts.poppins(
+    color: Colors.white,
+  );
+
+ TextStyle subtitleTextStyle = GoogleFonts.poppins(
+    color: subtitleColor,
+  );
+
+ TextStyle priceTextStyle = GoogleFonts.poppins(
+    color: priceColor,
+  );
+
+ TextStyle purpleTextStyle = GoogleFonts.poppins(
+    color: primaryColor,
+  );
+
+ TextStyle blackTextStyle = GoogleFonts.poppins(
+    color: blackColor,
+  );
+
+ TextStyle alertTextStyle = GoogleFonts.poppins(
+    color: alertColor,
+  );
+
+ FontWeight light = FontWeight.w300;
+ FontWeight regular = FontWeight.w400;
+ FontWeight medium = FontWeight.w500;
+ FontWeight semiBold = FontWeight.w600;
+ FontWeight bold = FontWeight.w700;
