06991b52f1dc9d4ebd8b5fdb2f70c08e65c6b0e9
Pramudya <pramudyap333@gmail.com>
2025-01-08 13:00:35 +0700

    implement provider in main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:transaction_tracker/form_page.dart';
 import 'package:transaction_tracker/home_page.dart';
 import 'package:transaction_tracker/model/transaction.dart';
-
 import 'package:transaction_tracker/boxes.dart';
+import 'package:transaction_tracker/provider/transaction_providers.dart';
 
 void main() async {
   // initialize hive flutter
@@ -12,7 +13,7 @@ void main() async {
 
   // registering adapter
   Hive.registerAdapter(TransactionAdapter());
-  
+
   // openbox
   boxTransactions = await Hive.openBox<Transaction>('transactionBox');
 
@@ -25,18 +26,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (context) => TransactionProvider(boxTransactions)),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routes: {
+          '/': (context) => const HomePage(),
+          '/form-page': (context) => const FormPage(),
+          '/home': (context) => const HomePage(),
+        },
       ),
-      routes: {
-        '/' : (context) => const HomePage(),
-        '/form-page' : (context) => const FormPage(),
-        '/home' : (context) => const HomePage(),
-      },
     );
   }
 }
